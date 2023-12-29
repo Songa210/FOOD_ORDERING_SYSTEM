13cca96176b9eef90bd2dad0a06779cdba365da5
Elyse NIYOMWUNGERE <elysesonga1997@gmail.com>
2023-12-29 15:57:35 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './router/router.jsx'
+
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />,
+)
