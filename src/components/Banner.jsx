d113f0cf2dc81fda4836ef4f8bedb05030d0ac16
Elyse NIYOMWUNGERE <elysesonga1997@gmail.com>
2024-01-03 18:17:26 +0200

    add navbar

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="max-w-screen-2x1 container max-auto xl:px-24 px-4">
+      Banner
+    </div>
+  );
+};
+
+export default Banner;
